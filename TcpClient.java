303006769a9ab2c049c3a15d7eac218e9ce14bf7
Adriene <adrienC562@gmail.com>
2015-02-17 15:19:30 -0800

    Halfday Checkpoint

--- TcpClient.java
+++ TcpClient.java
@@ -1,9 +1,85 @@
+//Adriene Cuenco
+//Initial branch
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.nio.ByteBuffer;
+
+import javax.xml.bind.DatatypeConverter;
+
+	
 public class TcpClient {
-
+    //Initialize Global Variables
+    static byte version = 4;
+    static byte hlen = 5;
+    static byte tos = 0;
+    static short tLen = 0;
+    static short iden = 0;
+    static int flags =0x4000;
+    static byte ttl = 50;
+    static byte protocol = 6; //tcp = 6
+    static short checksum = 0;
+    static int srcAddress = 0xC0A811A; //192.168.1.26
+    static int destAddress = 0x4C5B7B61; //76.91.123.97
+    /*
+    static OutputStream out;
+    static InputStream in;
+    */
+    
 	public static void main(String[] args) {
+		try(Socket socket = new Socket("76.91.123.97", 38005)){
+			OutputStream out = socket.getOutputStream();
+			InputStream in = socket.getInputStream();
+			
+		}
+		catch(Exception e){
+			System.out.println("Socket is closed or does not exist.");
+			System.out.println("Exiting program...");
+		}
 		System.out.println("Initial Branch... Lets do this!!");
 
-	}
+	} // End Main
+	
+	public static byte[] generateIpv4(byte[] data){
+		byte[] header = new byte[20 + data.length];
+		//reset checksum
+		checksum = 0;
+	
+		//Wrap header in Bytebuffer
+		ByteBuffer headerBuf = ByteBuffer.wrap(header);
+		headerBuf.put((byte) ((version & 0xf) << 4 | hlen & 0xf));
+		headerBuf.put(tos);
+        tLen = (short)(20 + data.length);
+        headerBuf.putShort(tLen);
+        headerBuf.putShort(iden);
+        headerBuf.putShort((short) flags);
+        headerBuf.put(ttl);
+        headerBuf.put(protocol);
+        headerBuf.putShort(checksum);
+        headerBuf.putInt(srcAddress);
+        headerBuf.putInt(destAddress);
+        checksum = (byte) checksum_Funct(headerBuf,hlen);
+        headerBuf.put(data);		
+		return header;
+	} //End Function generateIpv4
+	
+	public static byte[] generateTcp(byte data[]){
+		
+	}// end Funtion generateTcp
+	
+	public static short checksum_Funct(ByteBuffer bb, byte hlen){
+	    int num = 0;
+	    bb.rewind();
+	    for(int i = 0; i < hlen*2; ++i){
+	      num += 0xFFFF & bb.getShort();
+	    }
+	    num = ((num >> 16) & 0xFFFF) + (num & 0xFFFF);
+	    checksum = (short) (~num & 0xFFFF);
+	    bb.putShort(10,checksum);
+	    return checksum;
+	}//end checksum_Funct
+	
+	
 
-}
+} // End Class TcpClient
