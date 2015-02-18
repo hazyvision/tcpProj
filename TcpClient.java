8126c04375c596db7afdf09f9c7fa8cac981ed4f
Adriene <adrienC562@gmail.com>
2015-02-17 16:10:24 -0800

    Deadcode

--- TcpClient.java
+++ TcpClient.java
@@ -28,17 +28,29 @@ public class TcpClient {
     */
     
 	public static void main(String[] args) {
-		try(Socket socket = new Socket("76.91.123.97", 38005)){
+		try(Socket socket = new Socket("76.91.123.97", 38006)){
 			OutputStream out = socket.getOutputStream();
 			InputStream in = socket.getInputStream();
 			
+			int dataSize = 2;
+			//Creating syn packet-----------------------------------------------------------
+			byte[] data = generateRandomData(dataSize); 
+			byte[] tcpHeader = generateTcp(data);
+			byte[] ipv4Header = generateIpv4(tcpHeader);
+			
+			out.write(ipv4Header);
+			
+			byte[] synPacket = new byte[4];
+			in.read(synPacket);
+			//ByteBuffer synPacketBuf = ByteBuffer.wrap(synPacket);
+			System.out.println("synPacket> " +  DatatypeConverter.printHexBinary(synPacket));
+			//SYN packet complete------------------------------------------------------------
+				
 		}
 		catch(Exception e){
-			System.out.println("Socket is closed or does not exist.");
-			System.out.println("Exiting program...");
+			System.out.println("Incorrect address or port number.");
+			System.out.println("Socket is closing...");
 		}
-		System.out.println("Initial Branch... Lets do this!!");
-
 	} // End Main
 	
 	public static byte[] generateIpv4(byte[] data){
@@ -65,8 +77,31 @@ public class TcpClient {
 	} //End Function generateIpv4
 	
 	public static byte[] generateTcp(byte data[]){
+		byte[] header = new byte[20 + data.length];
+		ByteBuffer headerBuf = ByteBuffer.wrap(header);
+		checksum = 0;
 		
-	}// end Funtion generateTcp
+		int sequenceNum = 3232;
+		byte dataOffset = 5;
+		byte reserved = 0;
+		short windowSize = 0;
+		short urgentPointer = 0;
+		
+		headerBuf.putShort((short)0x420);//Random Source Port
+		headerBuf.putShort((short)0x013); //Random Dest Port
+		headerBuf.putInt(sequenceNum);   //Random sequence number
+		headerBuf.putInt(sequenceNum + 1); // Acknowledgment number (if ACK set)
+		headerBuf.put((byte) ((dataOffset & 0xF) << 4 | reserved & 0xF) ); //NS flag = 0(Built in reserve)
+		
+		//Flags --> [cwr,ece,urg,ack,psh,rst,syn,fin]
+		headerBuf.put((byte) 0b00000010);
+		headerBuf.putShort(windowSize);
+		headerBuf.putShort(checksum);
+		headerBuf.putShort(urgentPointer);
+		checksum_Funct(headerBuf,hlen);
+		headerBuf.put(data);
+		return header;
+	}// end Function generateTcp
 	
 	public static short checksum_Funct(ByteBuffer bb, byte hlen){
 	    int num = 0;
@@ -79,6 +114,15 @@ public class TcpClient {
 	    bb.putShort(10,checksum);
 	    return checksum;
 	}//end checksum_Funct
+	
+	public static byte[] generateRandomData(int size){
+		Random r = new Random();
+		byte[] randomArr = new byte[size];
+		for (int i = 0; i < size; i++){
+			randomArr[i] = (byte)r.nextInt();
+		}
+		return randomArr;
+	} // end Function generateRandomData
 	
 	
 
